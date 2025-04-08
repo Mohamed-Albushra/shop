dbd49188cc12077b9f458a7374d6b5665fd9d507
Mohamed-Albushra <mohamedalbushra39@gmail.com>
2025-04-08 11:37:13 +0200

    feat: landing page with brands bar and fonts

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -6,15 +6,15 @@ import NavbarSearch from '@/components/NavbarSearch';
 export default function Navbar() {
   return (
     <div className='flex gap-5 items-center justify-between h-20'>
-        <ul className='flex gap-10 space-x-3 pl-25'>
-            <li><Link href={'/'}>Home</Link></li>
-            <li><Link href={'/products'}>Products</Link></li>
-            <li><Link href={'/about'}>About</Link></li>
-        </ul>
-        <NavbarSearch/>
-        <Link href={'/cart'} className='pr-25'>
-            <ShoppingCart/>
-        </Link>
+      <ul className='flex gap-10 space-x-3 pl-25'>
+        <li><Link href={'/'}>Home</Link></li>
+        <li><Link href={'/products'}>Products</Link></li>
+        <li><Link href={'/about'}>About</Link></li>
+      </ul>
+      <NavbarSearch />
+      <Link href={'/cart'} className='pr-10 md:pr-5'>
+        <ShoppingCart />
+      </Link>
     </div>
   )
 }
