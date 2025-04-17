dd119c8cc79613e902ee8d57f462eb031419010b
Mohamed-Albushra <mohamedalbushra39@gmail.com>
2025-04-17 20:42:07 +0200

    fix: fixing build error

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import Navbar from "../components/Navbar";
 import { CartProvider } from "@/components/cartProvider";
 
+
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
