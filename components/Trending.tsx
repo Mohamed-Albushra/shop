320efc03ad283ce8896c259dc03cc57c206deaad
Mohamed-Albushra <mohamedalbushra39@gmail.com>
2025-04-29 10:27:03 +0200

    fit: add trending products section.

---
+++ components/Trending.tsx
@@ -0,0 +1,23 @@
+import { getAllProducts } from '@/lib/actions';
+import React from 'react'
+import { FocusCards } from "@/components/ui/focus-cards";
+import localFont from 'next/font/local';
+
+const manolo = localFont({ src: "../assets/manolo-mono.ttf" });
+async function Trending() {
+    const allProducts = await getAllProducts();
+    const sortedProducts = allProducts.sort((a, b) => b.rating - a.rating);
+    const top6Products = sortedProducts.slice(0, 6);
+    console.log(top6Products);
+
+  return (
+    <div className='bg-zinc-200 w-full p-3'>
+        <ul>
+        <h1 className={`${manolo.className} flex justify-center items-center text-3xl text-black pb-3 backdrop-opacity-10 px-4  rounded md:px-10 my-20`}>Trending Products</h1>
+        <FocusCards cards={top6Products.map((product) => ({ src: product.thumbnail, title: product.title }))} />
+        </ul>
+    </div>
+  )
+}
+
+export default Trending
